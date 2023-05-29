f40a004e693254dbf996b54d7a4d0ac0a0e2c46b
RayanAnser <rayan.anser@hotmail.fr>
2023-05-29 17:09:10 +0200

    Création des controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'users/show'
+  get 'offers/index'
+  get 'offers/new'
+  get 'offers/show'
+  get 'offers/edit'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
