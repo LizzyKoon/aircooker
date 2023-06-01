34da00efc7c7cd7e5de53f9e0a7ccd7f4372a298
RayanAnser <rayan.anser@hotmail.fr>
2023-06-01 17:58:17 +0200

    Dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  
+
   def home
     @offers = Offer.all
   end
 
   def dashboard
+    @user = current_user
+    @offers = current_user.offers
+    # @booking = Booking.find(params[:id])
+    # @booking.user = current_user
+    @bookings = Booking.where(user_id: current_user.id)
   end
 end
